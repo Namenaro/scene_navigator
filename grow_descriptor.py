09a21928525a91b72ef498410f6a6b838a4e32f2
namenaro <nanenaro@gmail.com>
2023-08-07 14:23:59 +0300

    tggjkgkjhg

---
+++ grow_descriptor.py
@@ -0,0 +1,52 @@
+from scene import Scene
+from utils import get_mini_ECG
+
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import text
+
+
+class Grower:
+    def __init__(self, signal):
+        self.scene = Scene(signal)
+
+    def step(self):
+        # находим пик ошибки на кущей сцене
+        # если там есть точка, то назначаем рабочей ее. Иначе -стаивм новую
+        current_point_name = self._select_current_point()
+
+        # для текущей рабочей точки определеяем горизонт ее действия
+        #  этом горизонтке находим индексы-кандидаты
+        indexes = self._select_indexes_candidates()
+
+        # для каждого кандидата делаем слепок сцены с добавленнымс сегментом и замеряем q
+        scene_winner = self._get_winner_candidate(indexes)
+
+        # выбираем победителя и заменяем текущую сцену на него
+        self.scene = scene_winner
+
+    def _select_current_point(self):
+        return point_name
+
+    def _select_indexes_candidates(self):
+        return indexes
+
+    def _get_winner_candidate(self, index_candidates):
+        return scene_winner
+
+
+
+
+if __name__ == '__main__':
+    signal = get_mini_ECG()
+
+
+    grower = Grower(signal)
+
+    # логгер шагов роста: каждый шаг в хтмл (визуальная отладка)
+    for i in range(4):
+        grower.step()
+        fig, axs = plt.subplots()
+        grower.scene.draw(axs)
+
+        plt.show()
